85ea917b9bfcabf4d0a932dd4da9f2cfd4afa82b
Alejandro Ruete <alejandro@greensway.se>
2020-08-26 12:57:41 +0200

    not cran

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -3,14 +3,20 @@ context("Test searching functions")
 thischeck=function() {
     test_that("search_fulltext generally functions as expected", {
         skip_on_cran()
-        expect_that(search_fulltext("Vulpes vulpes"),has_names(c("meta","data")))
-        expect_true(all(c("guid","name","commonName","rank","author","occurrenceCount") %in% names(search_fulltext("Vulpes vulpes")$data))) ## "score" and "isAustralian" also used to be present, but are no longer
+        expect_that(search_fulltext("Vulpes vulpes"),
+                    has_names(c("meta","data")))
+        expect_true(all(c("guid","name","commonName","rank",
+                          "author","occurrenceCount") %in% 
+                          names(search_fulltext("Vulpes vulpes")$data))) ## "score" and "isAustralian" also used to be present, but are no longer
         expect_that(nrow(search_fulltext("bilbobaggins")$data),equals(0)) ## query that should not match anything
         expect_that(nrow(search_fulltext("red",page_size=20)$data),equals(20))
-        expect_that(search_fulltext("Vulpes vulpes",output_format="complete"),has_names(c("meta","data")))
-        expect_that(search_fulltext("kingdom:Fungi",output_format="complete"),has_names(c("meta","data")))
+        expect_that(search_fulltext("Vulpes vulpes",output_format="complete"),
+                    has_names(c("meta","data")))
+        expect_that(search_fulltext("kingdom:Fungi",output_format="complete"),
+                    has_names(c("meta","data")))
         expect_output(print(search_fulltext("Vulpes vulpes")),"Search results:")
-        expect_output(print(search_fulltext("Vulpes vulpes",output_format="complete")),"nameFormatted") ## expect extra cols here
+        expect_output(print(search_fulltext("Vulpes vulpes",output_format="complete")),
+                      "nameFormatted") ## expect extra cols here
     })
 }
 check_caching(thischeck)
@@ -38,13 +44,15 @@ thischeck=function() {
         expect_error(search_fulltext("red",page_size=10,sort_by="blurg"))
         ## sort by scientific name
         ## note that ALA sorting is case-sensitive, with A-Z preceding a-z
-        temp <- search_fulltext("red",page_size=10,sort_by="scientificName")$data$scientificName
+        temp <- search_fulltext("red",page_size=10,
+                                sort_by="scientificName")$data$scientificName
         temp <- temp[grepl("^[A-Z]",temp)]
         expect_equal(order(temp),1:length(temp))
         ## descending order
-        temp <- search_fulltext("red",page_size=10,sort_by="scientificName",sort_dir="desc")$data$scientificName
+        temp <- search_fulltext("red",page_size=10,sort_by="scientificName",
+                                sort_dir="desc")$data$scientificName
         temp <- temp[grepl("^[A-Z]",temp)]
-        expect_equal(order(temp),length(temp):1)
+        expect_equal(order(temp), length(temp):1)
     })
 }
 check_caching(thischeck)
@@ -55,9 +63,13 @@ thischeck=function() {
     test_that("search_layers generally works as expected", {
         skip_on_cran()
         expect_that(search_layers(type="all"),is_a('data.frame'))
-        expect_that(search_layers(type="all",output_format="complete"),is_a('data.frame'))
+        expect_that(search_layers(type="all",
+                                  output_format="complete"),
+                    is_a('data.frame'))
         expect_that(nrow(search_layers(type="all")),is_more_than(50))
-        expect_that(nrow(search_layers(type="all",query="bilbobaggins")),equals(0))
+        expect_that(nrow(search_layers(type="all", 
+                                       query="bilbobaggins")),
+                    equals(0))
         expect_error(search_layers(type="bilbobaggins"))
         tmp <- search_layers(type = "shapes", query="ekoregioner")
         expect_lt(nchar(tmp$shortName),nchar(tmp$name))
@@ -72,7 +84,8 @@ check_caching(thischeck)
 thischeck=function() {
     test_that("search_names can cope with factor inputs", {
         skip_on_cran()
-        expect_equal(search_names(factor("Grevillea humilis")),search_names("Grevillea humilis"))
+        expect_equal(search_names(factor("Grevillea humilis")),
+                     search_names("Grevillea humilis"))
     })
 }
 check_caching(thischeck)
@@ -90,10 +103,10 @@ check_caching(thischeck)
 thischeck=function() {
     test_that("unexpected case-related behaviour in search_names has not changed", {
         skip_on_cran()
-        expect_equal(search_names("Leuctra digitata")$name,"Leuctra digitata Kempny, 1899")
-        expect_equal(search_names("Leuctra Digitata")$name,"Leuctra digitata Kempny, 1899")
-        expect_equal(search_names("Leuctra digitat")$name,as.character(NA))
-        expect_equal(search_names("Leuctra Digitat")$name,as.character(NA))
+        expect_equal(search_names("Leuctra digitata")$name, "Leuctra digitata Kempny, 1899")
+        expect_equal(search_names("Leuctra Digitata")$name, "Leuctra digitata Kempny, 1899")
+        expect_equal(search_names("Leuctra digitat")$name, as.character(NA))
+        expect_equal(search_names("Leuctra Digitat")$name, as.character(NA))
     })
 }
 check_caching(thischeck)
@@ -101,7 +114,8 @@ check_caching(thischeck)
 thischeck=function() {
     test_that("nonbreaking spaces not present in names", {
         skip_on_cran()
-        expect_false(any(colSums(apply(search_fulltext("Gallirallus australis")$data,2,function(z)grepl("\ua0",z)))>0))
+        expect_false(any(colSums(apply(search_fulltext("Gallirallus australis")$data,
+                                       2,function(z)grepl("\ua0",z)))>0))
         expect_false(grepl("\ua0",search_names("Gallirallus australis")$name))
     })
 }
