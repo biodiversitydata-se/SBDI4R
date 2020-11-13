c5736587fafc5dc12f9ff39238e3ef375c2b2ebe
Alejandro Ruete <alejandro@greensway.se>
2020-11-13 15:53:31 +0100

    new website

--- tests/testthat/test-species_list.R
+++ tests/testthat/test-species_list.R
@@ -3,7 +3,7 @@ context("Test specieslist")
 thischeck=function() {
     test_that("specieslist returns empty data.frame for no results", {
         skip_on_cran()
-        x <- specieslist("dfjknnaklhkjf",wkt="POLYGON((-3 56,-4 56,-4 57,-3 57,-3 56))",fq="rank:species")
+        x <- species_list("dfjknnaklhkjf",wkt="POLYGON((-3 56,-4 56,-4 57,-3 57,-3 56))",fq="rank:species")
         expect_is(x,"data.frame")
         expect_equal(nrow(x),0)
     })
@@ -14,7 +14,7 @@ thischeck=function() {
         x <- specieslist(wkt="POLYGON((-3 56,-4 56,-4 57,-3 57,-3 56))",fq="genus:Leuctra")
         expect_gt(nrow(x),0)
         skip("Not working on SBDI 500 status code error not warning")
-        expect_warning(x <- specieslist(wkt="POLYGON((-3 56,-4 56,-4 57,-3 57,-3 56))",fq="rk_genus:Leuctra"))
+        expect_warning(x <- species_list(wkt="POLYGON((-3 56,-4 56,-4 57,-3 57,-3 56))",fq="rk_genus:Leuctra"))
         expect_equal(nrow(x),0)
     })
 }
@@ -22,7 +22,7 @@ check_caching(thischeck)
 
 thischeck = function() {
   test_that("specieslist arguments in SBDI4R package match arguments in ALA4R package", {
-    expect_named(formals(specieslist),names(formals(ALA4R::specieslist)),ignore.order = TRUE)
+    expect_named(formals(species_list),names(formals(ALA4R::specieslist)),ignore.order = TRUE)
   })
 }
 check_caching(thischeck)
